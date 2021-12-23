3930d74f70ee48a70916092a6e1006856c3fc7fb
Brandon Lanier <brandonjlanier@gmail.com>
2021-12-22 22:48:13 -0600

    updated for of loop to include a let for the variable

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -51,7 +51,7 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
-for (supply of supplyChanges) {
+for (let supply of supplyChanges) {
   if (supply > 0) {
     console.log(`Added ${supply} parts.`);
   } if (supply === 0) {
@@ -60,6 +60,7 @@ for (supply of supplyChanges) {
     console.log(`Removed ${supply} parts.`);
   }
 }
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
